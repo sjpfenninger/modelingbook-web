479d73efa3bf9cbd1f5719e6b794a6b37fea249b
Stefan Pfenninger <stefan@pfenninger.org>
2011-11-02 08:55:44 +0100

    More local library versions; various fixes

--- 2box.js
+++ 2box.js
@@ -1,5 +1,5 @@
 function setUpUI() {
-    function updateBoxSize() {
+    var updateBoxSize = function() {
         jsPlumb.repaintEverything();
         tangle.setValue("box1size", $("#modelbox1").width() * $("#modelbox1").height());
         tangle.setValue("box2size", $("#modelbox2").width() * $("#modelbox2").height());
@@ -8,32 +8,53 @@ function setUpUI() {
     $('.modelbox')
         .resizable({ resize: function(event, ui) {updateBoxSize()}, autoHide: true, grid: [20, 20] });
     jsPlumb.draggable($('.modelbox'));
-    $('.modelbox').draggable("option", {"containment":'parent', "cursor":'hand', "opacity":'0.5'});
-    jsPlumb.connect({
-    	source:"modelbox1", 
-    	target:"modelbox2",
-    	anchors:["RightMiddle", "LeftMiddle" ],
-    	overlays:[ 
-        		["Arrow", { width:12, length:10, location:1} ],
-        		[ "Label", { label:'<span data-var="boxconnect" class="TKAdjustableNumber inputField" data-min="0" data-max="1" data-step="0.01" data-format="%.2f"></span>', location:0.3, cssClass:"connectorlabel"} ]
-        	],
-    	endpoint:"Dot",
+    $('.modelbox').draggable("option", {"containment":'parent', "cursor":'hand', "opacity":'0.5', });
+    var common = {
+    	endpoint:"Blank",
     	endpointStyle:{ fillStyle: "" },
     	connector:"Straight",
     	paintStyle:{ strokeStyle:"#727272", lineWidth:2 }
-    });
-    firstbox1resize = true;
-	$('#modelbox1').bind( "resize", function(event, ui) {
-	    if (firstbox1resize) {
-            $('#modelbox2').css("top","20px");
+    };
+    jsPlumb.connect({
+    	source:"modelbox1", 
+    	target:"modelbox2",
+    	anchors:[[1, 0.25, 1, 0], [0, 0.25, -1, 0]],
+    	overlays:[ 
+        		["Arrow", { width:12, length:10, location:1} ],
+        		[ "Label", { label:'<span data-var="box1to2" class="TKAdjustableNumber inputField" data-min="0" data-max="1" data-step="0.01" data-format="%.2f"></span>', location:0.5, cssClass:"connectorlabel connectorlabel1"} ]
+        	],
+    }, common);
+    jsPlumb.connect({
+    	source:"modelbox2", 
+    	target:"modelbox1",
+    	anchors:[[0, 0.75, -1, 0], [1, 0.75, 1, 0]],
+    	overlays:[ 
+        		["Arrow", { width:12, length:10, location:1} ],
+        		[ "Label", { label:'<span data-var="box2to1" class="TKAdjustableNumber inputField" data-min="0" data-max="1" data-step="0.01" data-format="%.2f"></span>', location:0.5, cssClass:"connectorlabel connectorlabel2"} ]
+        	],
+    }, common);
+    firstboxresize = true;
+	$('.modelbox').bind( "resize", function(event, ui) {
+	    if (firstboxresize) {
+            $('#modelbox2').css("top","70px");
         }
-        firstbox1resize = false;
+        firstboxresize = false;
+    });
+}
+
+function updateUIElements () {
+    $("span.inputField span").each(function () {
+        if (Number($(this).text()) == 0) {
+          $(this).parent().parent().addClass("eqInactive");
+        } else {
+          $(this).parent().parent().removeClass("eqInactive");
+        }
     });
 }
 
 function setUpTangle() {
 
-	var initialized = false;
+	var tangleInitialized = false;
 
 	var element = document.getElementById("modelboxcontainer");
 
@@ -43,22 +64,20 @@ function setUpTangle() {
             this.box1out = 0.8;
 			this.box2in = 0.008;
 			this.box2out = 1.0;
-			this.boxconnect = 1.0;
+			this.box1to2 = 1.0;
+			this.box2to1 = 0;
 			this.box1size = $("#modelbox1").width() * $("#modelbox1").height();
 			this.box2size = $("#modelbox2").width() * $("#modelbox2").height();
         },
         update: function () {
-			if (initialized) {
+			if (tangleInitialized) {
 			    
 			}
-			if (this.k4 > 0) {
-				$("span#k4_container").removeClass("eqInactive");
-			} else {
-				$("span#k4_container").addClass("eqInactive");
-			}
-        }
+            updateUIElements();
+        },
     });
 	
-	initialized = true;
+	updateUIElements();
+	tangleInitialized = true;
 
 }
