77eae9f38820c1cb72542d03d72b03a5d1909276
Stefan Pfenninger <stefan@pfenninger.org>
2011-11-02 18:52:22 +0100

    Box-internal variable behavior niceified

--- 2box.js
+++ 2box.js
@@ -21,7 +21,7 @@ function setUpUI() {
     	anchors:[[1, 0.25, 1, 0], [0, 0.25, -1, 0]],
     	overlays:[ 
         		["Arrow", { id: 'arrow', width:12, length:10, location:1} ],
-        		[ "Label", { id: 'label', label:'<span data-var="box1to2" class="TKAdjustableNumber inputField" data-min="0" data-max="1" data-step="0.01" data-format="%.2f"></span>', location:0.5, cssClass:"connectorlabel label_box1_box2"} ]
+        		[ "Label", { id: 'label', label:'<span data-var="box1to2" class="TKAdjustableNumber inputField" data-min="0" data-max="1" data-step="0.01" data-format="%.2f"></span>', location:0.45, cssClass:"connectorlabel label_box1_box2"} ]
         	],
     }, common);
     connector_box2_box1 = jsPlumb.connect({
@@ -30,7 +30,7 @@ function setUpUI() {
     	anchors:[[0, 0.75, -1, 0], [1, 0.75, 1, 0]],
     	overlays:[ 
         		["Arrow", { id: 'arrow', width:12, length:10, location:1} ],
-        		[ "Label", { id: 'label', label:'<span data-var="box2to1" class="TKAdjustableNumber inputField" data-min="0" data-max="1" data-step="0.01" data-format="%.2f"></span>', location:0.5, cssClass:"connectorlabel label_box2_box1"} ]
+        		[ "Label", { id: 'label', label:'<span data-var="box2to1" class="TKAdjustableNumber inputField" data-min="0" data-max="1" data-step="0.01" data-format="%.2f"></span>', location:0.55, cssClass:"connectorlabel label_box2_box1"} ]
         	],
     }, common);
     firstboxresize = true;
@@ -44,11 +44,20 @@ function setUpUI() {
 
 function updateUIElements () {
     $("span.inputField span").each(function () {
-        if (Number($(this).text()) == 0) {
-          $(this).parent().parent().addClass("eqInactive");
+        var fieldNumber = Number($(this).text());
+        var fieldContainer = $(this).parent().parent();
+        // Next two lines are hack-ish, probably there is a much better way:
+        // If there is any data in data.('former') take that (even if it's '0'), else, initialize it with fieldNumber
+        var former_fieldNumber = $(this).data('former') || fieldNumber
+        if ($(this).data('former') == 0) {former_fieldNumber = 0;}
+        if (fieldNumber == 0) {
+            fieldContainer.addClass("eqInactive");
         } else {
-          $(this).parent().parent().removeClass("eqInactive");
+            fieldContainer.removeClass("eqInactive");
+            if (fieldNumber > 0) {fieldContainer.addClass("eqPositive");fieldContainer.removeClass("eqNegative");}
+            if (fieldNumber < 0) {fieldContainer.addClass("eqNegative");fieldContainer.removeClass("eqPositive");}
         }
+        $(this).data('former', fieldNumber);
     });
 }
 
@@ -61,7 +70,7 @@ function setUpTangle() {
     tangle = new Tangle(element, {
         initialize: function () {
             this.box1in = 0.5;
-            this.box1out = 0.8;
+            this.box1out = 0.1;
 			this.box2in = 0.008;
 			this.box2out = 1.0;
 			this.box1to2 = 1.0;
