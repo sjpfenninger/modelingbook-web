25b08d43d06aa1041f024d0edb44b09ef04de734
Stefan Pfenninger <stefan@pfenninger.org>
2012-04-16 18:59:29 +0200

    Added require.js; updated some libs; compiled css files to .gitignore

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+require([
+    "order!libraries/jquery-1.7.2.js",
+    "order!libraries/jquery-ui-1.8.18.custom.min.js",
+    "order!libraries/bootstrap-dropdown.js",
+    "order!libraries/Tangle.js",
+    "order!libraries/TangleKit/mootools.js",
+    "order!libraries/TangleKit/sprintf.js",
+    "order!libraries/TangleKit/BVTouchable.js",
+    "order!libraries/TangleKit/TangleKit.js",
+    "libraries/jquery.jsPlumb-1.3.8-all.js",
+    "libraries/jsxgraphcore.js",
+    "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
+], function () {
+    // $(document).ready(function(){
+        // $('#topbar').dropdown();
+    // });
+});
